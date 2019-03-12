23a928f7a0776f5d8c20281d85512e5c8c9fe044
Michael Reardon <michael.hdi@gmail.com>
2019-03-12 15:05:21 -0400

    dog model/dogcontroller functional

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 var express = require('express');
 var app = express();
+var dogs = require('./controllers/dogsController');
 
 var user = require('./controllers/userController');
 var sequelize = require('./db');
@@ -13,6 +14,10 @@ app.use(require('./middleware/headers'));
 
 //EXPOSED ROUTES//
 app.use('/dogs', user);
+
+//PROTECTED ROUTES//
+app.use(require('./middleware/validate-session'));
+app.use('/dogs', dogs);
 
 app.listen(3000, function(){
     console.log('!!! RED_ROCKET 3000 !!!')
